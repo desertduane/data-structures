9b8cd2feec2eed9a07b22e5ff38006070dcbfeba
duane rush <desertduane@yahoo.com>
2017-09-12 17:02:11 -0700

    reversing strings

---
+++ strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = "";
+
+            for(int i = input.Length - 1; i>=0; i--)
+            {
+                output += input[i];
+            }
+            Console.WriteLine(output);
+            Console.Read();
+        }
+    }
+}
