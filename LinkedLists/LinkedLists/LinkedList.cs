1b45ccde45c7d6feda141d7393f19fdf3174f67c
duane rush <desertduane@yahoo.com>
2017-09-18 18:13:16 -0700

    linked-lists

---
+++ LinkedLists/LinkedLists/LinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedLists
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(string data)
+        {
+            Node toAdd = new Node();
+            toAdd.data = data;
+
+
+            if (head == null)
+            {
+                toAdd.next = head;
+                head = toAdd;
+            }
+            else
+            {
+                Node current = head;
+                
+
+                //Console.WriteLine(current.data);
+                while (current.next != null)
+                {
+                    current = current.next;
+                    //Console.WriteLine(current.data);
+                }
+                current.next = toAdd;
+
+            }
+
+        }
+        public void printList() {
+
+            if (head == null)
+            {
+                Console.WriteLine("list is empty"); 
+            }
+            else
+            {
+                Node current = head;
+                Console.WriteLine(current.data);
+                while (current.next != null)
+                {
+                    current = current.next;
+                    Console.WriteLine(current.data);
+                }
+
+            }
+        }
+
+    }
+}
