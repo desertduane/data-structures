6b64bd410b4a6744581053d05814dfbbefdff029
duane rush <desertduane@yahoo.com>
2017-09-19 17:18:54 -0700

    middle found

---
+++ LinkedListFindMiddle/LinkedListMiddle/LinkedListMiddle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LinkedListMiddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList duanesDopeList = new LinkedList();
+            duanesDopeList.Add("ONE");
+            duanesDopeList.Add("TWO");
+            duanesDopeList.Add("THREE");
+            duanesDopeList.Add("FOUR");
+            duanesDopeList.Add("Five");
+            duanesDopeList.Add("SIX");
+            duanesDopeList.Add("SEVEN");
+            duanesDopeList.Add("EIGHT");
+            duanesDopeList.Add("NINE");
+            duanesDopeList.printList();
+            Console.WriteLine();
+            duanesDopeList.printMiddle();
+            Console.Read();
+        }
+    }
+}
---
+++ LinkedLists/LinkedLists/README (2).md
@@ -0,0 +1,5 @@
+Our approach for this assignment was to start by psudo coding it out the best we could. 
+We then went and did some research online to find some simple examples of linked-lists 
+until we found one that looked like it was what we were wanting to do and used it as a 
+helping guide to build out our linked list. Once we had it build was just testing and debugging
+to get it to do what we wanted. 
