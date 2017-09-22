d836842f9e9319777fa89c7f3d7fcb4a61ee3738
duane rush <desertduane@yahoo.com>
2017-09-21 17:11:06 -0700

    LinkedListDelete

---
+++ LinkedListDelete/LinkedListDelete/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListsDelete
+{
+    public class Node
+    {
+        public Node Next;
+        public Object Data;
+
+        public Node(Object data) => Data = data;
+    }
+
+}
---
+++ LinkedListDelete/README.md
@@ -0,0 +1,3 @@
+We wrote out our goal on the table and came to a general idea on how to impliment it. We then 
+refactored our previous find middle method to allow us to delete the node that we gave it. Our method
+skips over the node that we tell it to. 
