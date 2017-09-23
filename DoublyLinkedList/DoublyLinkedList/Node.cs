3d712f4508af278bc533c0eaff723162bc7a817a
duane rush <desertduane@yahoo.com>
2017-09-22 20:12:26 -0700

    DoublyLinked

---
+++ DoublyLinkedList/DoublyLinkedList/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedList
+{
+    class Node
+    {
+        public Node Previous;
+        public Node Next;
+        public Object Data;
+
+        public Node(Object data) => Data = data;
+    }
+}
