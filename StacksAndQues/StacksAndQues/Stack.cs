11db6fce3d471351801ad17a6f2a39a796cfadaa
duane rush <desertduane@yahoo.com>
2017-09-26 21:58:30 -0700

    stacks and queues

---
+++ StacksAndQues/StacksAndQues/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQues
+{
+    class Stack
+    {
+       public Node Tail;
+
+       public void Push(object data)
+        {
+            Node newNode = new Node
+            {
+                Data = data
+            };
+            if(Tail == null)
+            {
+                //first node both head and tail
+                Tail = newNode;
+                Console.WriteLine(Tail.Data);
+            }
+            else
+            {
+                //makes each new node the new tail and writes it to console
+                Tail.Next = newNode;
+                newNode.Previous = Tail;
+                Tail = newNode;
+                Console.WriteLine(Tail.Data);
+            }
+        }
+        public void Pop()
+        {
+            if(Tail == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            //removes tail node and sets previous node as new tail.
+            Node removed = Tail;
+            Tail = removed.Previous;
+            Tail.Next = null;
+            Console.WriteLine(Tail.Data);
+
+        }
+        
+    }
+
+}
