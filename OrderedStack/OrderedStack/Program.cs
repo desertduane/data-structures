5b40cfb9274100e5bda4561efb0dc482867cb30d
duane rush <desertduane@yahoo.com>
2017-09-29 21:37:57 -0700

    ordered stack

---
+++ OrderedStack/OrderedStack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OrderedStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stackList = new Stack();
+            stackList.SortedPush(3);
+            stackList.SortedPush(2);
+            stackList.SortedPush(4);
+            stackList.SortedPush(6);
+            stackList.SortedPush(8);
+            stackList.SortedPush(7);
+            stackList.Pop();
+            stackList.View();
+            Console.Read();
+        }
+    }
+}
