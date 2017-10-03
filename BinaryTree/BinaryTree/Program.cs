a002e584c443cf2c27d27ce4cce3d38f62b2ea8f
duane rush <desertduane@yahoo.com>
2017-10-02 18:25:46 -0700

    Binary Tree

---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree myTree = new Tree();
+            myTree.Root.Left = new Leaf(1);
+            myTree.Root.Right = new Leaf(2);
+            myTree.Root.Left.Left = new Leaf(3);
+            myTree.Root.Left.Right = new Leaf(4);
+            myTree.Root.Right.Left = new Leaf(5);
+            myTree.Root.Right.Right = new Leaf(6);
+
+            Console.WriteLine(myTree.Root.Right.Right.Data);
+            Console.WriteLine(myTree.Root.Data);
+            Console.Read();
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/README.md
@@ -0,0 +1,10 @@
+﻿How long did it take you to complete this assignment?
+about an hour.
+What did you struggle with? Why? How did you solve?
+The only thing we struggled on was trying to work on an add method
+over adding manually. We do not have it solved as of yet
+What did you learn during this assignment?
+I learned the basic structure of a binary tree and its 
+basically what it sounds like it is.
+What resources did you utilize for this assingment?
+Google and my code challenge partners. 
