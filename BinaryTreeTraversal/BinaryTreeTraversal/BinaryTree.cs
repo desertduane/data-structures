149f5db591cf4947e692b8de5165bc4b5aa3b16c
duane rush <desertduane@yahoo.com>
2017-10-03 21:05:36 -0700

    Binary tree

---
+++ BinaryTreeTraversal/BinaryTreeTraversal/BinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeTraversal
+{
+    class BinaryTree
+    {
+        public Node Root;
+
+        public BinaryTree() => Root = new Node(null);
+
+        public void GetChildren(Node Current)
+        {
+            Console.WriteLine(Current.Data);
+            if(Current.Left != null) { GetChildren(Current.Left); }
+            if(Current.Right != null) { GetChildren(Current.Right); }
+            return;
+        }
+    }
+
+
+    public class Node
+    {
+        public object Data;
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public Node Current { get; set; }
+
+        public Node(object data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+
+        }
+    }
+}
---
+++ BinaryTreeTraversal/BinaryTreeTraversal/README.md
@@ -0,0 +1,11 @@
+﻿How long did it take you to complete this assignment?
+1 hour 30 minutes
+What did you struggle with? Why? How did you solve?
+I was overthinking the problem and was going down rabbit holes 
+I that were dead ends. Brandon helped us realize its much simpler than 
+we thought.
+What did you learn during this assignment?
+I learned I easily overthink these data structures and back myself into
+a wall.
+What resources did you utilize for this assingment?
+Google and classmates
