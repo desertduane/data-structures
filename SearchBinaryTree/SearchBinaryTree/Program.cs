5f63995c264d375bb205881ecf20e8be5abab4f1
duane rush <desertduane@yahoo.com>
2017-10-18 21:47:41 -0700

    delete method in and reademe done

--- SearchBinaryTree/SearchBinaryTree/Program.cs
+++ SearchBinaryTree/SearchBinaryTree/Program.cs
@@ -13,17 +13,29 @@ namespace SearchBinaryTree
             testTree.Add(testTree.Root, 6);
             testTree.Add(testTree.Root, 5);
             testTree.Add(testTree.Root, 4);
+
+            testTree.Root = testTree.Delete(testTree.Root, 8);
+
             Console.WriteLine();
+
+
+
             testTree.InOrder(testTree.Root);
+
             Console.WriteLine();
+
             testTree.PostOrder(testTree.Root);
             Console.WriteLine();
+
             testTree.PreOrder(testTree.Root);
             Console.WriteLine();
+
             testTree.Breadth(testTree.Root);
             Console.WriteLine();
+
             testTree.Min(testTree.Root);
             testTree.Max(testTree.Root);
+            
 
             Console.Read();
         }
