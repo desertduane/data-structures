63a9e0d6743b75bf200cfb64db816565f5c2c488
duane rush <desertduane@yahoo.com>
2017-10-23 21:40:19 -0700

    InsertSort structure

---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 4, 5, 2, 7, 8, 3 };
+            InsertionSort(numbers);
+            Console.Read();
+
+
+        }
+        static int[] InsertionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int n = i + 1; n > 0; n--)
+                {
+                    if (arr[n - 1] > arr[n])
+                    {
+                        int temp = arr[n - 1];
+                        arr[n - 1] = arr[n];
+                        arr[n] = temp;
+                    }
+                }
+            }
+            foreach(int n in arr)
+            {
+                Console.WriteLine(n);
+            }
+            return arr;
+        }
+    }
+}
---
+++ InsertionSort/InsertionSort/README.txt
@@ -0,0 +1,12 @@
+﻿What did you learn?
+I had no idea what was meant by an insertion sort was prior to this 
+challenge. Upon investiaging I learned that this sort method is only 
+usefull on small arrays or lists and gets painfully slow on larger arrays 
+
+
+Where did you look?
+https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-6.php
+
+What notes did you use to help you successfully complete the challenge?
+The website above was my guide to figuring out a the challenge and writing it out 
+on the whiteboard from memory helped me understand what it is doing. 
