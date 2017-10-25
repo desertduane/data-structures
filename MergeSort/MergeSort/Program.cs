7a415b5ee7dc33351f2229f262e9c0163df67055
duane rush <desertduane@yahoo.com>
2017-10-24 18:32:12 -0700

    Merge Sort method

---
+++ MergeSort/MergeSort/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 3, 8, 7, 5, 12, 2, 1 };
+
+            int length = 8;
+           
+            Sort(numbers, 0, length - 1);
+
+            foreach(int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+            Console.Read();
+        }
+
+        static public void Sort(int[] numbers, int left, int right)
+        {
+            int mid = (right + left) / 2;
+                
+            if (right > left)
+            {
+                //recursivly breaks arry left half
+                Sort(numbers, left, mid);
+                //recursivly breaks array right half
+                Sort(numbers, (mid + 1), right);
+                Merge(numbers, left, (mid + 1), right);
+
+            }
+        }
+
+        static public void Merge(int[] arr, int left, int mid, int right)
+        {
+           int[] temp = new int[arr.Length];
+
+           int leftEnd = (mid - 1);
+           int tempPos = left;
+           int numElements = (right - left + 1);
+
+            while (left <= leftEnd && mid <= right)
+            {
+                if (arr[left] <= arr[mid])
+                {
+                    temp[tempPos++] = arr[left++];
+                }
+                else
+                {
+                    temp[tempPos++] = arr[mid++];
+                }
+            }
+
+            while (left <= leftEnd)
+            {
+                temp[tempPos++] = arr[left++];
+            }
+
+            while (mid <= right)
+            {
+                temp[tempPos++] = arr[mid++];
+            }
+
+            for (int i = 0; i < numElements; i++)
+            {
+                arr[right] = temp[right];
+                right--;
+            }
+        }
+    }
+}
---
+++ MergeSort/MergeSort/README.txt
@@ -0,0 +1,11 @@
+﻿What did you learn?
+Prior to this challenge I had no idea what a merge sort was but from this exercise I 
+learned how the merge sort method breaks appart an array and puts it back together sorted.
+
+Where did you look?
+http://www.sanfoundry.com/csharp-program-merge-sort/
+https://www.wikiwand.com/en/Merge_sort
+
+What notes did you use to help you successfully complete the challenge?
+No notes just reading the above sites to understand what was needing to be done and to 
+get an example of a merge sort method to understand what it is doing. 
